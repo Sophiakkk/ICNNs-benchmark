ce556713d36789b7febd239776b01d586baaf24d
Sophiakkk <zhuzhuhsm@gmail.com>
2023-10-05 21:28:06 -0400

    improve 1d code

---
+++ 1D_benchmark/grammy_icnns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_1d_benchmarks
+#SBATCH --output=train_1d_benchmarks.log
+#SBATCH --error=train_1d_benchmarks.err
+#SBATCH --mem=8GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:2
+
+T_list=(50 100)
+for tmax in ${T_list[@]}
+    python train.py -T $tmax
