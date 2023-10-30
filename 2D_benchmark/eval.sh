ff5425e62eab8cfe1af0b513613d047835563507
Sophiakkk <zhuzhuhsm@gmail.com>
2023-10-30 11:05:20 -0400

    add evaluation shell file

---
+++ 2D_benchmark/eval.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=eval_2d_benchmarks
+#SBATCH --output=eval_2d_benchmarks.log
+#SBATCH --error=eval_2d_benchmarks.err
+#SBATCH --mem=8GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=cpu:4
+
+python evaluation.py >> output.txt
