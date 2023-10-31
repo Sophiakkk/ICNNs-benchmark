8e9f8c12fb0c6d0fdcc7677ecefe006cc0adde98
Sophiakkk <zhuzhuhsm@gmail.com>
2023-10-31 18:52:51 -0400

    improve eval shell

--- 2D_benchmark/eval.sh
+++ 2D_benchmark/eval.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=train_2d_benchmarks
-#SBATCH --output=train_2d_benchmarks.log
-#SBATCH --error=train_2d_benchmarks.err
+#SBATCH --job-name=eval_2d_benchmarks
+#SBATCH --output=eval_2d_benchmarks.log
+#SBATCH --error=eval_2d_benchmarks.err
 #SBATCH --mem=8GB
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
