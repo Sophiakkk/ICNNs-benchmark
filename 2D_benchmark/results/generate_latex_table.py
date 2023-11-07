6c0302ee3055bb4a1d2976d0e411f5bf6b36f459
Sophiakkk <zhuzhuhsm@gmail.com>
2023-11-07 17:05:11 -0500

    create summary latex file

--- 2D_benchmark/results/generate_latex_table.py
+++ 2D_benchmark/results/generate_latex_table.py
@@ -1,27 +1,10 @@
-headers = ["ackley","bukin","tray","dropwave","eggholder","griewank","holdertable",
+T_list = [100]
+method_list = ['ICNNs']
+func_list = ["ackley","bukin","tray","dropwave","eggholder","griewank","holdertable","langermann",
              "levy","levy13","rastrigin","schaffer2","schaffer4","schwefel", "shubert"]
-
-with open ("summary_2d_T50.txt", "r") as f:
-    data50 = {}
-    for line in f:
-        if line.startswith("Senario"):
-            func = line.split()[4]
-            data50[func] = []
-        if line.startswith("avg error (output)"):
-            data50[func].append(line.split()[3])
-        if line.startswith("std error (output)"):
-            data50[func].append(line.split()[3])
-
-with open ("summary_2d_T100.txt", "r") as f:
-    data100 = {}
-    for line in f:
-        if line.startswith("Senario"):
-            func = line.split()[4]
-            data100[func] = []
-        if line.startswith("avg error (output)"):
-            data100[func].append(line.split()[3])
-        if line.startswith("std error (output)"):
-            data100[func].append(line.split()[3])
+seed_list = [1,2,3,4,5,6,7,8,9,10]
+lr_list = [1e-3]
+# lr_list = [1e-3,1e-4,1e-5,1e-6,1e-7]
 
 with open("summary_all.txt", "r") as gdf:
     GD = {}
@@ -34,17 +17,43 @@ with open("summary_all.txt", "r") as gdf:
         if line.startswith("std error (output)"):
             GD[func].append(line.split()[3]) 
 
+data_list = [] 
+for tmax in T_list:
+    for k in range(tmax//20+1):
+        t = k*20
+        for lr in lr_list:
+            with open ('summary_2d_T{}_t{}_lr{}.txt'.format(tmax,t,lr), "r") as f:
+                data = {}
+                for line in f:
+                    if line.startswith("Senario"):
+                        func = line.split()[4]
+                        data[func] = []
+                    if line.startswith("avg error (output)"):
+                        data[func].append(line.split()[3])
+                    if line.startswith("std error (output)"):
+                        data[func].append(line.split()[3])
+                data_list.append(data)
+
 with open ("latex_summary.txt", "w") as file:
     file.write("\\begin{table}\n")
     file.write("\\centering\n")
-    file.write("\\caption{Benchmark Optimization Errors: this table shows y Euclidien distance}\n")
+    file.write("\\caption{")
+    file.write("Benchmark Optimization Errors for lr{}".format(str(lr_list[0])))
+    file.write("}\n")
     file.write("\\label{benchmark_yerrors_icnns}\n")
     file.write("\\resizebox{\\textwidth}{!}{\n")
-    file.write("\\begin{tabular}{c|ccc}\n")
+    file.write("\\begin{tabular}{c|ccccccc}\n")
     file.write("\hline\n")
-    file.write("Test Senerio & GD & ICNNs(T50) & ICNNs(T100) \\\ \hline\n")
-    for func in headers:
-       file.write(func + " & " + "${} \pm {} $".format(GD[func][0],GD[func][1]) + " & " + "${} \pm {} $".format(data50[func][0], data50[func][1]) + " & " + "${} \pm {} $".format(data100[func][0], data100[func][1]) + "\\\ \n")
+    file.write("Test Senerio & GD ")
+    for j in range(len(data_list)):
+        file.write("& ICNNs(t={})".format(str(j*20)))
+    file.write("\\\ \hline\n")
+    for func in func_list:
+        row = func + " & " + "${} \pm {} $".format(GD[func][0],GD[func][1])
+        for i in range(len(data_list)):
+            row +=  " & " + "${} \pm {} $".format(data_list[i][func][0], data_list[i][func][1])
+        row += "\\\ \n"
+        file.write(row)
     file.write("\hline\n")
     file.write("\end{tabular}}\n")
     file.write("\end{table}\n")
